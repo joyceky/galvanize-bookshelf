f11778ccad0ed92793a08b724705c9d8e30f041e
Kyle Joyce <kyyjoyce@gmail.com>
2016-12-01 16:34:55 -0700

    seed test 4 still passing :wrench:

--- seeds/3_favorites.js
+++ seeds/3_favorites.js
@@ -3,12 +3,14 @@
 exports.seed = function(knex, Promise){
   return knex('favorites').del()
   .then(() => {
-    return knex('favorites').insert([{
+    return Promise.all([
+    knex('favorites').insert([{
       id: 1,
       book_id: 1,
       user_id: 1,
       created_at: new Date('2016-06-29 14:26:16 UTC'),
       updated_at: new Date('2016-06-29 14:26:16 UTC')
-    }]);
+    }])
+   ]);
   });
 };
